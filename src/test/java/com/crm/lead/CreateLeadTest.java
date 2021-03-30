0435319f2d0bfc67b8752ceb565401eea7464573
PraveenR090 <praveensurya090@gmail.com>
2021-03-30 14:53:22 +0530

    abc

---
+++ src/test/java/com/crm/lead/CreateLeadTest.java
@@ -0,0 +1,17 @@
+package com.crm.lead;
+
+import org.testng.annotations.Test;
+
+public class CreateLeadTest {
+@Test(groups="smoke")
+public void createlead()
+{
+	System.out.println("execute create lead test");
+}
+
+@Test(groups="regression")
+public void createleadwithcontact()
+{
+	System.out.println("execyyryyyyryry");
+}
+}
---
+++ src/test/java/com/crm/lead/Test.java
@@ -0,0 +1,12 @@
+package com.crm.lead;
+
+public class Test {
+	
+	@org.testng.annotations.Test
+	public void test(){
+		String name = System.getProperty("browser");
+		System.out.println(name);
+		
+	}
+
+}
